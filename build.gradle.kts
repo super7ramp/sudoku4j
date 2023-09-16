f54290895afccd90c696708ce71525026694a1a3
Antoine Belvire <antoine.belvire@opensuse.org>
2023-09-16 21:45:59 +0200

    Update JUnit

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 
 dependencies {
     implementation("org.ow2.sat4j:org.ow2.sat4j.core:2.3.4")
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
