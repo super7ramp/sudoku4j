b376f2ff342d2a4dd68049ca97f6b075be20324d
Antoine Belvire <antoine.belvire@opensuse.org>
2023-09-18 17:52:57 +0200

    Bump Sat4j version to 2.3.6
    2.3.4 is the version advertised on Sat4j product page
    but 2.3.6 is the latest version. It notably fixes the
    module information.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,6 @@
 
 plugins {
     java
-    id("org.javamodularity.moduleplugin") version "1.8.12"
 }
 
 group = "re.belv"
@@ -16,7 +15,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.ow2.sat4j:org.ow2.sat4j.core:2.3.4")
+    implementation("org.ow2.sat4j:org.ow2.sat4j.core:2.3.6")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
